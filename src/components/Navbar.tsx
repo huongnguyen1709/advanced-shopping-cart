f1a56aaf8ed66d7d0ac5b350beadb1b383473156
Huong <huongnguyen.nth1709@gmail.com>
2024-01-17 16:42:28 +0200

    create Navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+import { Navbar as NavbarBs, Container } from 'react-bootstrap';
+
+export const Navbar = () => {
+  return (
+    <NavbarBs className='bg-white shadow-sm mb-3'>
+      <Container>Nav</Container>
+    </NavbarBs>
+  );
+};
