43727a3e261d035f2fa5b9f1f4c39877a99bec8a
Huong <huongnguyen.nth1709@gmail.com>
2024-01-29 15:23:01 +0200

    map data json in Store component

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,5 +1,19 @@
-import React from 'react';
+import { Row, Col } from 'react-bootstrap';
+import storeItems from '../data/items.json';
+import StoreItem from '../components/StoreItem';
 
 export const Store = () => {
-  return <div>Store</div>;
+  console.log(storeItems);
+  return (
+    <>
+      <div>Store</div>
+      <Row md={2} xs={1} lg={3} className='g-3'>
+        {storeItems.map((item) => (
+          <Col key={item.id}>
+            <StoreItem {...item} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 };
